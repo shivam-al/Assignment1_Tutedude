beb7d838eaf456073154664ec944bba4f2a1ee9d
Shivam Kumar Mishra <137914957+shivam-al@users.noreply.github.com>
2026-02-11 00:36:44 +0530

    Add files via upload

---
+++ Assignment 1- Task2.py
@@ -0,0 +1,9 @@
+"""Task 2: Create a Personalized Greeting
+Problem Statement: Write a Python program that:
+1.  Takes a user's first name and last name as input.
+2.  Concatenates the first name and last name into a full name.
+3.  Prints a personalized greeting message using the full name.
+"""
+c= input("Enter your first name: ")
+d= input("Enter your last name: ")
+print(f"Hello, {c}  {d}! Welcome to the Python program.")
---
+++ Assignment 1-Task1.py
@@ -0,0 +1,18 @@
+"""Task 1: Perform Basic Mathematical Operations
+Problem Statement: Write a Python program that does the following:
+1.  Takes two numbers as input from the user.
+2.  Performs the basic mathematical operations on these two numbers:
+o	Addition
+o	Subtraction
+o	Multiplication
+o	Division
+3.  Displays the results of each operation on the screen."""
+
+a= int(input("Enter the first number: "))
+b=int(input("Enter the second number:  "))
+
+print(f"Addition: {a+b}")
+print(f"Subtraction: {a-b} ")
+print(f"Multiplication: {a*b}")
+print(f"Division: {a/b}")
+
